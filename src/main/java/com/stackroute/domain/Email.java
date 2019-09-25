3f98e0046bfa2b0524f9abefbcab13fe29f799b1
Jayashree <jayashree.a@cgi.com>
2019-09-25 16:43:41 +0530

    added jenkins file

---
+++ src/main/java/com/stackroute/domain/Email.java
@@ -0,0 +1,13 @@
+package com.stackroute.domain;
+
+import lombok.*;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Email{
+    private String to;
+    private String Subject;
+    private String text;
+}
